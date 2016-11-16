31f8a6be9096a912096bcf6babd475347a568c76
adithyabhatkajake <dth.bht@gmail.com>
2016-11-16 18:06:20 +0530

    cleanup

---
+++ Exercises/Exercise10/.tmp_versions/pen-drive.mod
@@ -0,0 +1,2 @@
+/media/adithya/ADITHYA/Programs/Kernel-Modules/K-rack/Exercises/Exercise10/pen-drive.ko
+/media/adithya/ADITHYA/Programs/Kernel-Modules/K-rack/Exercises/Exercise10/pen-drive.o
--- Exercises/Exercise10/pen-drive.c
+++ Exercises/Exercise10/pen-drive.c
@@ -1,46 +1,98 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/usb.h>
- 
-static int pen_probe(struct usb_interface *interface, const struct usb_device_id *id)
+
+static void __exit pdrive_exit(void);
+static int __init pdrive_init(void);
+
+module_init(pdrive_init);
+module_exit(pdrive_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Adithya Bhat <dth.bht@gmail.com>");
+MODULE_DESCRIPTION("USB Driver in the making");
+
+/*
+ *  Some module initializations.
+ */
+
+static int the_pen_prober (  struct          usb_interface *interface,
+                            const struct    usb_device_id *id )
 {
-    printk(KERN_INFO "Pen drive (%04X:%04X) plugged\n", id->idVendor, id->idProduct);
+    /*
+     *  What the device should do in the system kernel when
+     *  a new device is detected. Kernel initialization for
+     *  the device is done here.
+     *  The probe function is only called only when no driver
+     *  is handling the device.
+     *  In other words, if another driver is responsible for
+     *  the device, then the probe function is not called.
+     */
+    pr_info ("Pen drive (%04X:%04X) plugged\n", id->idVendor, id->idProduct);
     return 0;
 }
- 
-static void pen_disconnect(struct usb_interface *interface)
+
+static void the_pen_disconnector ( struct usb_interface *interface )
 {
-    printk(KERN_INFO "Pen drive removed\n");
+    /*
+     *  What the driver should do in the kernel when it's
+     *  device is removed from the system.
+     *  Kernel memory cleanup for the device is done here.
+     */
+    pr_info("Pen drive removed\n");
 }
- 
-static struct usb_device_id pen_table[] =
+
+static struct usb_device_id table_of_pen_drive_ids[] =
 {
-    { USB_DEVICE(0x0781,0x5590) },
+    /*
+     *  USB_DEVICE Macro takes as argument, the vendor in hexadecimal
+     *  followed by the id in hexadecimal
+     */
+    {
+        USB_DEVICE(0x930,0x6545)
+    },
     {} /* Terminating entry */
 };
+
 MODULE_DEVICE_TABLE (usb, pen_table);
- 
-static struct usb_driver pen_driver =
+
+
+/*
+ *  The global structure containing the USB Driver's Core
+ *  functionalities defined, used during the registration.
+ */
+
+static struct usb_driver my_driver =
 {
-    .name = "pen_driver",
-    .id_table = pen_table,
-    .probe = pen_probe,
-    .disconnect = pen_disconnect,
+    .name = "The super awesome K-Rack pen drive",
+    .id_table = table_of_pen_drive_ids,
+    .probe = the_pen_prober,
+    .disconnect = the_pen_disconnector,
 };
- 
-static int __init pen_init(void)
+
+static int __init pdrive_init(void)
 {
-    return usb_register(&pen_driver);
+    /*
+     *  On inserting the module, register itself to detect
+     *  any device with the given id.
+     */
+    int ret;
+    pr_info ("Registering module\n");
+    ret = usb_register(&my_driver);
+    if (ret != 0 ){
+        pr_alert ("Registration failed.\n");
+        return ret;
+    }
+    pr_info ("Registration successful\n");
+    return ret;
 }
- 
-static void __exit pen_exit(void)
+
+/*
+ *  Upon exit, one must always cleanup.
+ *  Freeing kernel memory is very important.
+ */
+
+static void __exit pdrive_exit(void)
 {
     usb_deregister(&pen_driver);
 }
- 
-module_init(pen_init);
-module_exit(pen_exit);
- 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Anil Kumar Pugalia <email_at_sarika-pugs_dot_com>");
-MODULE_DESCRIPTION("USB Pen Registration Driver");
